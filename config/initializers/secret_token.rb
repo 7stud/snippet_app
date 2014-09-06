3618e01deaf1749ace4c4ff93dff9029965491f6
7stud <7stud@excite.com>
2014-09-05 23:26:40 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SnipettApp::Application.config.secret_key_base = '7c51de3763350d938bfa47b9e91ca91b8e2b121b5c558f3a382a1260b2b8ff6d00dbef7e2b4751853a6952d5edcdaf2dc4324bbd574ee9d00a226f6c65daf771'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SnipettApp::Application.config.session_store :cookie_store, key: '_snipett_app_session'
